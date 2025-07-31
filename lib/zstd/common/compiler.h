3fe8479c05251bff9d30146f59c236c920d7ce2f
ELF <3580977309@qq.com>
2025-07-31 09:55:20 +0800

    compiler.h

--- lib/zstd/common/compiler.h
+++ lib/zstd/common/compiler.h
@@ -169,7 +169,7 @@
  * - CPP17: https://en.cppreference.com/w/cpp/language/attributes/fallthrough
  * - Else: __attribute__((__fallthrough__))
  */
-#define ZSTD_FALLTHROUGH __attribute__((__fallthrough__))
+#define ZSTD_FALLTHROUGH do { __attribute__((__fallthrough__)); } while (0)
 
 /*-**************************************************************
 *  Alignment
